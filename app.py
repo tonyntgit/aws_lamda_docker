ea65bbe2d6390dcccc0cf5cd663c77bbd2aa3c71
tonyntgit <116912787+tonyntgit@users.noreply.github.com>
2022-10-28 11:24:09 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import json
+import requests
+
+def handler(event, context):
+    username = event['username']
+    
+    user_data = github.get_user_detail(username)
+    return {
+        'statusCode': 200,
+        'body': json.dumps(user_data)
+    }
+
+def get_user_detail(username: str):
+    user_data = requests.get('https://api.github.com/users/%s' % username).json()
+
+    return user_data
